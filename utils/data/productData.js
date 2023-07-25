7bed5bee05aac4181483da524becbac7587a5433
Wesley Vance <wslyvnce@gmail.com>
2023-07-25 16:49:15 -0500

    adding product promises

--- utils/data/productData.js
+++ utils/data/productData.js
@@ -22,4 +22,42 @@ const updateProduct = (product) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-export default getAllProducts;
+const getSingleProduct = (productId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/products/${productId}`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getProductsBySellerId = (sellerId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/products?seller_id=${sellerId}`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getProductsByCategoryId = (categoryId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/products?category_id=${categoryId}`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const postProduct = (uid, product) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/products`, {
+    method: 'POST',
+    body: JSON.stringify(
+      product,
+    ),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => resolve(response.json()))
+    .catch(reject);
+});
+
+export {
+  updateProduct, getAllProducts, getProductsBySellerId,
+},
